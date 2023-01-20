560be15732abfdf6314500d22fd213c1ebb821d0
unknown <thomasbarenghi@gmail.com>
2023-01-20 13:28:54 -0300

    first

---
+++ backend/src/api/alumno/routes/alumno.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * alumno router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::alumno.alumno');
---
+++ backend/src/api/alumno/services/alumno.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * alumno service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::alumno.alumno');
