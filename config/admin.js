8946d8b2df779b73721815461a166d42e8b526d9
unknown <thomasbarenghi@gmail.com>
2023-01-20 15:47:17 -0300

    ok

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('9PZ5l8jj92XeocxPMU19Hw'),
+    secret: '9PZ5l8jj92XeocxPMU19Hw',
   },
   apiToken: {
     salt: 'oc2m9HtLRAINZTijD4cwig',
