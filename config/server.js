31075d0a351e8a89907419ee39fe1eeb5f2162ac
unknown <thomasbarenghi@gmail.com>
2023-01-20 15:15:19 -0300

    ok

--- config/server.js
+++ config/server.js
@@ -1,10 +1,9 @@
-APP_KEYS=['hufaLk7DhIsJ5UddAceHuQ','uP1xoPCD0qJDBh3T2371sw']
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['hufaLk7DhIsJ5UddAceHuQ','uP1xoPCD0qJDBh3T2371sw'],
   },
 });
 
-keys: [['hufaLk7DhIsJ5UddAceHuQ','uP1xoPCD0qJDBh3T2371sw']]
