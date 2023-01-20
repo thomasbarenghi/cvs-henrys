7fd02e4c5a0d6f748bd4d3bdec0340569de36f3b
unknown <thomasbarenghi@gmail.com>
2023-01-20 16:49:41 -0300

    hola

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,9 @@
+const crypto = require('crypto');
+
 module.exports = ({ env }) => ({
-    // ... other configs
-    plugins: {
-      // ... other plugin configs
-      'users-permissions': {
-        jwtSecret: "k87+YP+KO5k9WwDRPg7p8g"
-      }
-    }
-  });
-  
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
+    },
+  },
+});
