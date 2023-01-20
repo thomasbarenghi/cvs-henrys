377a5537a5b56184ee204a77eacfce48a95b7499
unknown <thomasbarenghi@gmail.com>
2023-01-20 13:56:51 -0300

    ok

--- frontend/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '@/styles/global.css'
-import '@/styles/global-fonts.css'
-import '@/styles/global-vars.css'
-import '@/styles/bootstrap.min.css'
-
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
