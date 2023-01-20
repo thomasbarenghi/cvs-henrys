3b8c3ff0390b2a29c41048623d110e80c468e11a
unknown <thomasbarenghi@gmail.com>
2023-01-20 16:37:43 -0300

    ok

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+    // ... other configs
+    plugins: {
+      // ... other plugin configs
+      'users-permissions': {
+        jwtSecret: "k87+YP+KO5k9WwDRPg7p8g"
+      }
+    }
+  });
+  
