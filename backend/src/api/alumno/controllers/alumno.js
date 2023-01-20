7623c27bf5b829a578bc755ae7ce9435bd5f0cfc
unknown <thomasbarenghi@gmail.com>
2023-01-20 14:20:22 -0300

    ok

---
+++ backend/src/api/alumno/controllers/alumno.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * alumno controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::alumno.alumno');
