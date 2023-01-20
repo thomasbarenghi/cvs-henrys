2eebf1b39bfe7c0d2aea6173a35f2ca43c5f5f10
unknown <thomasbarenghi@gmail.com>
2023-01-20 15:28:12 -0300

    ok

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('9PZ5l8jj92XeocxPMU19Hw'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: 'oc2m9HtLRAINZTijD4cwig',
   },
 });
